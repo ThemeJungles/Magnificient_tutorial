cac3b057d916318cc595a51120ffb79463eb025a
Nirmalya Ghosh <nirmalya.email@gmail.com>
2015-08-29 01:31:49 +0530

    Updates

--- homepage.php
+++ homepage.php
@@ -7,53 +7,294 @@ Template Name: Custom Homepage
 <?php get_header(); ?>
 
 			<!-- Row for main content area -->
-			<div class="small-12 large-12 columns" id="page-content" role="main">
+			<div class="large-12 columns homepage" id="page-content" role="main">
+				
+				<!-- product-slider-container -->
+				<div class="product-slider-container">
 
-				<?php
-					
-					// Product Slider
-
-					// Getting  the images
-					$product_slider_image_1 = Kirki::get_option( mc, product_slider_image_1 );
-					$product_slider_image_2 = Kirki::get_option( mc, product_slider_image_2 );
-					$product_slider_image_3 = Kirki::get_option( mc, product_slider_image_3 );
-					$product_slider_image_4 = Kirki::get_option( mc, product_slider_image_4 );
-					$product_slider_image_5 = Kirki::get_option( mc, product_slider_image_5 );
-
-					// Check if any image exists
-					if ( $product_slider_image_1 || $product_slider_image_2 || $product_slider_image_3 || $product_slider_image_4 || $product_slider_image_5 ) {
-
-						// Code for flexslider
-				?>
+					<?php
 						
-						<div class="product-slider">
-					  		<ul class="slides">
-					    		<?php if ( $product_slider_image_1 ) { echo '<li><img src="'. $product_slider_image_1 .'" class="product-slider-image-1"></li>'; } ?>
-					   			<?php if ( $product_slider_image_2 ) { echo '<li><img src="'. $product_slider_image_2 .'" class="product-slider-image-2"></li>'; } ?>
-					   			<?php if ( $product_slider_image_3 ) { echo '<li><img src="'. $product_slider_image_3 .'" class="product-slider-image-3"></li>'; } ?>
-					   			<?php if ( $product_slider_image_4 ) { echo '<li><img src="'. $product_slider_image_4 .'" class="product-slider-image-4"></li>'; } ?>
-					   			<?php if ( $product_slider_image_5 ) { echo '<li><img src="'. $product_slider_image_5 .'" class="product-slider-image-5"></li>'; } ?>
-					  		</ul>
+						// Product Slider
+
+						// Getting the images
+						$product_slider_image_1 = Kirki::get_option( mc, product_slider_image_1 );
+						$product_slider_image_2 = Kirki::get_option( mc, product_slider_image_2 );
+						$product_slider_image_3 = Kirki::get_option( mc, product_slider_image_3 );
+						$product_slider_image_4 = Kirki::get_option( mc, product_slider_image_4 );
+						$product_slider_image_5 = Kirki::get_option( mc, product_slider_image_5 );
+
+						//Getting the captions
+						$product_slider_caption_1 = Kirki::get_option( mc, product_slider_caption_1 );
+						$product_slider_caption_2 = Kirki::get_option( mc, product_slider_caption_2 );
+						$product_slider_caption_3 = Kirki::get_option( mc, product_slider_caption_3 );
+						$product_slider_caption_4 = Kirki::get_option( mc, product_slider_caption_4 );
+						$product_slider_caption_5 = Kirki::get_option( mc, product_slider_caption_5 );
+
+
+						// Check if any image exists
+						if ( $product_slider_image_1 || $product_slider_image_2 || $product_slider_image_3 || $product_slider_image_4 || $product_slider_image_5 ) {
+
+							// Code for flexslider
+					?>
+							
+							<div class="product-slider">
+						  		<ul class="slides">
+						    		<?php if ( $product_slider_image_1 )
+						    			{
+						    		?>
+						    			<li>
+						    				<img src="<?php echo $product_slider_image_1; ?>" class="product-slider-image-1" />
+						    				<?php if ( $product_slider_caption_1 ) { echo '<p class="flex-caption">'. $product_slider_caption_1 .'</p>'; } ?>
+						    			</li>	
+						    		<?php
+						    			} 
+						    		?>
+						    		
+
+						   			<?php if ( $product_slider_image_2 )
+						    			{
+						    		?>
+						    			<li>
+						    				<img src="<?php echo $product_slider_image_2; ?>" class="product-slider-image-2" />
+						    				<?php if ( $product_slider_caption_2 ) { echo '<p class="flex-caption">'. $product_slider_caption_2 .'</p>'; } ?>
+						    			</li>	
+						    		<?php
+						    			} 
+						    		?>
+
+						    		<?php if ( $product_slider_image_3 )
+						    			{
+						    		?>
+						    			<li>
+						    				<img src="<?php echo $product_slider_image_3; ?>" class="product-slider-image-3" />
+						    				<?php if ( $product_slider_caption_3 ) { echo '<p class="flex-caption">'. $product_slider_caption_3 .'</p>'; } ?>
+						    			</li>	
+						    		<?php
+						    			} 
+						    		?>
+
+						    		<?php if ( $product_slider_image_4 )
+						    			{
+						    		?>
+						    			<li>
+						    				<img src="<?php echo $product_slider_image_4; ?>" class="product-slider-image-4" />
+						    				<?php if ( $product_slider_caption_4 ) { echo '<p class="flex-caption">'. $product_slider_caption_4 .'</p>'; } ?>
+						    			</li>	
+						    		<?php
+						    			} 
+						    		?>
+
+						    		<?php if ( $product_slider_image_5 )
+						    			{
+						    		?>
+						    			<li>
+						    				<img src="<?php echo $product_slider_image_5; ?>" class="product-slider-image-5" />
+						    				<?php if ( $product_slider_caption_5 ) { echo '<p class="flex-caption">'. $product_slider_caption_5 .'</p>'; } ?>
+						    			</li>	
+						    		<?php
+						    			} 
+						    		?>						  		
+						    		</ul>
+							</div>
+
+							<style type="text/css">
+								.flex-caption {
+								  	width: 100%;
+								  	padding: 2%;
+								  	text-align: center;
+								  	left: 0;
+								  	bottom: 0;
+								  	background: rgba(0,0,0,.5);
+								  	color: #fff;
+								  	text-shadow: 0 -1px 0 rgba(0,0,0,.3);
+								  	font-size: 14px;
+								  	line-height: 18px;
+								}
+							</style>
+
+							<script type="text/javascript">
+								(function($) {
+									$(window).load(function() {
+						  				$('.product-slider').flexslider({
+						    				animation: "slide",
+						    				controlNav: false,
+						    				directionNav: false,
+						    				slideshowSpeed: 3000, 
+						  				});
+									});
+								})(jQuery);
+							</script>
+
+					<?php
+						}
+
+					?>
+
+				</div>
+				<!-- /product-slider-container -->
+
+				<div class="separator"></div>
+				
+				<!-- description-boxes-container -->
+				<div class="description-boxes-container">
+					<div class="large-12 columns">
+						<div class="row" data-equalizer>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php 
+									$product_description_image_1 = Kirki::get_option( mc, product_description_image_1 );
+									$product_description_textarea_1 = Kirki::get_option( mc, product_description_textarea_1 );
+									if ( $product_description_image_1 ) {
+										echo '<img src="'. $product_description_image_1 .'" class="product-description-image">';
+									}
+									if ( $product_description_textarea_1 ) {
+										echo '<h6 class="product-description-textarea text-center">'. $product_description_textarea_1 .'</h6>';
+									}	
+								?>
+							</div>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php 
+									$product_description_image_2 = Kirki::get_option( mc, product_description_image_2 );
+									$product_description_textarea_2 = Kirki::get_option( mc, product_description_textarea_2 );
+									if ( $product_description_image_2 ) {
+										echo '<img src="'. $product_description_image_2 .'" class="product-description-image">';
+									}
+									if ( $product_description_textarea_2 ) {
+										echo '<h6 class="product-description-textarea text-center">'. $product_description_textarea_2 .'</h6>';
+									}
+								?>
+							</div>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php 
+									$product_description_image_3 = Kirki::get_option( mc, product_description_image_3);
+									$product_description_textarea_3 = Kirki::get_option( mc, product_description_textarea_3 );
+									if ( $product_description_image_3 ) {
+										echo '<img src="'. $product_description_image_3 .'" class="product-description-image">';
+									}
+									if ( $product_description_textarea_3 ) {
+										echo '<h6 class="product-description-textarea text-center">'. $product_description_textarea_3 .'</h6>';
+									}
+								?>
+							</div>
+
 						</div>
+					</div>
+				</div>
+				<!-- /description-boxes-container -->
 
-						<script type="text/javascript">
-							(function($) {
-								$(window).load(function() {
-					  				$('.product-slider').flexslider({
-					    				animation: "slide",
-					    				controlNav: true,
-					    				directionNav: false
-					  				});
-								});
-							})(jQuery);
-						</script>
+				<div class="separator"></div>
 
-				<?php
-					}
+				<!-- product-details-container -->
+				<div class="product-details-container">
+					<div class="large-12 columns">
+						
+						<?php
+							$product_details_image_1 = Kirki::get_option( mc, product_details_image_1 );
+							$product_details_textarea_1 = Kirki::get_option( mc, product_details_textarea_1 );
+							if ( $product_details_image_1 || $product_details_textarea_1 ) {
+						?>
+								<div class="row" data-equalizer id="product-details-one">
+									<div class="large-6 columns panel" data-equalizer-watch id="product-image">
+										<?php
+											if ( $product_details_image_1 ) {
+												echo '<img src="'. $product_details_image_1 .'" />';
+											}
+										?>
+									</div>
+									<div class="large-6 columns panel" data-equalizer-watch id="product-description">
+										<?php
+											if ( $product_details_textarea_1 ) {
+												echo '<h6>'. $product_details_textarea_1 .'</h6>';
+											}
+										?>
+									</div>
+								</div>
+						<?php
+							}
+						?>
 
-				?>
+						<?php
+							$product_details_image_2 = Kirki::get_option( mc, product_details_image_2 );
+							$product_details_textarea_2 = Kirki::get_option( mc, product_details_textarea_2 );
+							if ( $product_details_image_2 || $product_details_textarea_2 ) {
+						?>
+								<div class="row" data-equalizer id="product-details-two">
+									<div class="large-6 columns panel" data-equalizer-watch id="product-description">
+										<?php
+											if ( $product_details_textarea_2 ) {
+												echo '<h6>'. $product_details_textarea_2 .'</h6>';
+											}
+										?>
+									</div>
+									<div class="large-6 columns panel" data-equalizer-watch id="product-image">
+										<?php
+											if ( $product_details_image_2 ) {
+												echo '<img src="'. $product_details_image_2 .'" />';
+											}
+										?>
+									</div>
+								</div>
+						<?php
+							}
+						?>
+
+					</div>
+				</div>
+				<!-- /product-details-container -->
+
+				<div class="separator"></div>
+
+				<!-- team-showcase-container -->
+				<div class="team-showcase-container">
+					<div class="large-12 columns">
+						<div class="row" data-equalizer>
+
+							<?php
+								
+								// For team member 1
+								$team_showcase_image_1 = Kirki::get_option( mc, team_showcase_image_1 );
+								$team_showcase_text_1 = Kirki::get_option( mc, team_showcase_text_1 );
+
+								// For team member 2
+								$team_showcase_image_2 = Kirki::get_option( mc, team_showcase_image_2 );
+								$team_showcase_text_2 = Kirki::get_option( mc, team_showcase_text_2 );
+
+								// For team member 3
+								$team_showcase_image_3 = Kirki::get_option( mc, team_showcase_image_3 );
+								$team_showcase_text_3 = Kirki::get_option( mc, team_showcase_text_3 );
+
+							?>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php
+									if ( $team_showcase_image_1 ) { echo '<img src="'. $team_showcase_image_1 .'" />'; }
+									if ( $team_showcase_text_1 ) { echo '<h6 class="team-showcase-member text-center">'. $team_showcase_text_1 .'</h6>'; }
+								?>
+							</div>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php
+									if ( $team_showcase_image_2 ) { echo '<img src="'. $team_showcase_image_2 .'" />'; }
+									if ( $team_showcase_text_2 ) { echo '<h6 class="team-showcase-member text-center">'. $team_showcase_text_2 .'</h6>'; }
+								?>
+							</div>
+
+							<div class="large-4 columns panel" data-equalizer-watch>
+								<?php
+									if ( $team_showcase_image_3 ) { echo '<img src="'. $team_showcase_image_3 .'" />'; }
+									if ( $team_showcase_text_3 ) { echo '<h6 class="team-showcase-member text-center">'. $team_showcase_text_3 .'</h6>'; }
+								?>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<!-- /team-showcase-container -->
+
+				<div class="separator"></div>
 
 			</div>
-			<hr>
-
+			
 <?php get_footer(); ?>
--- library/theme-options.php
+++ library/theme-options.php
@@ -21,11 +21,72 @@ Kirki::add_panel( 'product_slider', array(
     'description' => __( 'A slider to show the products', 'magnificient' ),
 ) );
 
+// Adding the Product Description panel
+Kirki::add_panel( 'product_description', array(
+    'priority'    => 10,
+    'title'       => __( 'Product Description', 'magnificient' ),
+    'description' => __( 'Panels to show description of the products', 'magnificient' ),
+) );
+
+// Adding the Product Details panel
+Kirki::add_panel( 'product_details', array(
+    'priority'    => 10,
+    'title'       => __( 'Product Details', 'magnificient' ),
+    'description' => __( 'Panels to show details of the products', 'magnificient' ),
+) );
+
+// Adding the Team Showcase panel
+Kirki::add_panel( 'team_showcase', array(
+    'priority'    => 10,
+    'title'       => __( 'Team Showcase', 'magnificient' ),
+    'description' => __( 'A section to show your team', 'magnificient' ),
+) );
+
 // Adding the Product Slider for Homepage section
 Kirki::add_section( 'product_slider_for_homepage', array(
     'title'          => __( 'Product Slider for Homepage', 'magnificient' ),
     'description'    => __( 'This slider will be shown on the front page of your website', 'magnificient' ),
     'panel'          => 'product_slider', // Not typically needed.
+    'priority'       => 160,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+// Adding the Product Images for Homepage section
+Kirki::add_section( 'product_images_for_homepage', array(
+    'title'          => __( 'Product Images for Homepage', 'magnificient' ),
+    'description'    => __( 'These images will be shown on the product description panels on the  front page of your website', 'magnificient' ),
+    'panel'          => 'product_description', // Not typically needed.
+    'priority'       => 160,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+// Adding the Product Description for Homepage section
+Kirki::add_section( 'product_description_for_homepage', array(
+    'title'          => __( 'Product Description for Homepage', 'magnificient' ),
+    'description'    => __( 'These panels will be shown on the front page of your website', 'magnificient' ),
+    'panel'          => 'product_description', // Not typically needed.
+    'priority'       => 160,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+// Adding the Product Details for Homepage section
+Kirki::add_section( 'product_details_for_homepage', array(
+    'title'          => __( 'Product Details for Homepage', 'magnificient' ),
+    'description'    => __( 'This will show the details on the front page of your website', 'magnificient' ),
+    'panel'          => 'product_details', // Not typically needed.
+    'priority'       => 160,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+// Adding the Team Showcase for Homepage section
+Kirki::add_section( 'team_showcase_for_homepage', array(
+    'title'          => __( 'Team Showcase for Homepage', 'magnificient' ),
+    'description'    => __( 'This will show the team members on the front page of your website', 'magnificient' ),
+    'panel'          => 'team_showcase', // Not typically needed.
     'priority'       => 160,
     'capability'     => 'edit_theme_options',
     'theme_supports' => '', // Rarely needed.
@@ -86,13 +147,236 @@ Kirki::add_field( 'mc', array(
     'priority'    => 10,
 ) );
 
+// Adding the Product Slider caption 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'product_slider_caption_1',
+    'label'       => __( 'Product Slider caption 1', 'magnificient' ),
+    'description' => __( 'This caption will be the description for the first slide.', 'magnificient' ),
+    'section'     => 'product_slider_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
 
+// Adding the Product Slider caption 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'product_slider_caption_2',
+    'label'       => __( 'Product Slider caption 2', 'magnificient' ),
+    'description' => __( 'This caption will be the description for the second slide.', 'magnificient' ),
+    'section'     => 'product_slider_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
 
+// Adding the Product Slider caption 3 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'product_slider_caption_3',
+    'label'       => __( 'Product Slider caption 3', 'magnificient' ),
+    'description' => __( 'This caption will be the description for the third slide.', 'magnificient' ),
+    'section'     => 'product_slider_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
 
+// Adding the Product Slider caption 4 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'product_slider_caption_4',
+    'label'       => __( 'Product Slider caption 4', 'magnificient' ),
+    'description' => __( 'This caption will be the description for the fourth slide.', 'magnificient' ),
+    'section'     => 'product_slider_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
 
+// Adding the Product Slider caption 5 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'product_slider_caption_5',
+    'label'       => __( 'Product Slider caption 5', 'magnificient' ),
+    'description' => __( 'This caption will be the description for the fifth slide.', 'magnificient' ),
+    'section'     => 'product_slider_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
 
+// Adding the Product Description Image 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'product_description_image_1',
+    'label'       => __( 'Product Description image 1', 'magnificient' ),
+    'description' => __( 'This image will be the first image for the Product Description.', 'magnificient' ),
+    'section'     => 'product_images_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
 
+// Adding the Product Description Image 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'product_description_image_2',
+    'label'       => __( 'Product Description image 2', 'magnificient' ),
+    'description' => __( 'This image will be the second image for the Product Description.', 'magnificient' ),
+    'section'     => 'product_images_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
 
+// Adding the Product Description Image 3 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'product_description_image_3',
+    'label'       => __( 'Product Description image 3', 'magnificient' ),
+    'description' => __( 'This image will be the third image for the Product Description.', 'magnificient' ),
+    'section'     => 'product_images_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Product Description Textarea 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'textarea',
+    'settings'     => 'product_description_textarea_1',
+    'label'       => __( 'Product Description textarea 1', 'magnificient' ),
+    'description' => __( 'This description will be the description for the first product.', 'magnificient' ),
+    'section'     => 'product_description_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Product Description Textarea 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'textarea',
+    'settings'     => 'product_description_textarea_2',
+    'label'       => __( 'Product Description textarea 2', 'magnificient' ),
+    'description' => __( 'This description will be the description for the second product.', 'magnificient' ),
+    'section'     => 'product_description_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Product Description Textarea 3 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'textarea',
+    'settings'     => 'product_description_textarea_3',
+    'label'       => __( 'Product Description textarea 3', 'magnificient' ),
+    'description' => __( 'This description will be the description for the third product.', 'magnificient' ),
+    'section'     => 'product_description_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Product Details image 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'product_details_image_1',
+    'label'       => __( 'Product Details image 1', 'magnificient' ),
+    'description' => __( 'This image will be the first image for the Product Details.', 'magnificient' ),
+    'section'     => 'product_details_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Product Details image 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'product_details_image_2',
+    'label'       => __( 'Product Details image 2', 'magnificient' ),
+    'description' => __( 'This image will be the second image for the Product Details.', 'magnificient' ),
+    'section'     => 'product_details_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Product Details textarea 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'textarea',
+    'settings'     => 'product_details_textarea_1',
+    'label'       => __( 'Product Details textarea 1', 'magnificient' ),
+    'description' => __( 'This description will be the details for the first product.', 'magnificient' ),
+    'section'     => 'product_details_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Product Details textarea 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'textarea',
+    'settings'     => 'product_details_textarea_2',
+    'label'       => __( 'Product Details textarea 2', 'magnificient' ),
+    'description' => __( 'This description will be the details for the second product.', 'magnificient' ),
+    'section'     => 'product_details_for_homepage',
+    'default'     => __( 'This is some default text', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase image 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'team_showcase_image_1',
+    'label'       => __( 'Team Showcase image 1', 'magnificient' ),
+    'description' => __( 'This image will be the first image for the Team Showcase.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase image 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'team_showcase_image_2',
+    'label'       => __( 'Team Showcase image 2', 'magnificient' ),
+    'description' => __( 'This image will be the second image for the Team Showcase.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase image 3 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'image',
+    'settings'     => 'team_showcase_image_3',
+    'label'       => __( 'Team Showcase image 3', 'magnificient' ),
+    'description' => __( 'This image will be the third image for the Team Showcase.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => '',
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase text 1 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'team_showcase_text_1',
+    'label'       => __( 'Team Member 1', 'magnificient' ),
+    'description' => __( 'Name of the first member.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => __( 'John Doe', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase text 2 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'team_showcase_text_2',
+    'label'       => __( 'Team Member 2', 'magnificient' ),
+    'description' => __( 'Name of the second member.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => __( 'John Doe', 'magnificient' ),
+    'priority'    => 10,
+) );
+
+// Adding the Team Showcase text 3 field
+Kirki::add_field( 'mc', array(
+    'type'        => 'text',
+    'settings'     => 'team_showcase_text_3',
+    'label'       => __( 'Team Member 3', 'magnificient' ),
+    'description' => __( 'Name of the third member.', 'magnificient' ),
+    'section'     => 'team_showcase_for_homepage',
+    'default'     => __( 'John Doe', 'magnificient' ),
+    'priority'    => 10,
+) );
 
 
 ?>
